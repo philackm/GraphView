9244e9bed3f6ae2f8cbd58830e16464894542090
jz709u <jz709u@gmail.com>
2017-02-01 19:35:59 +0900

    Selectable Nodes Delegate
    -PointSelectedProtocol triggers on touch of node or bar
    -changes to touchEnded when user lifts off at any points or bars then
    return the label, value, and the point at which selected
    -this is used for display for purposes or for presenting a popup at the
    point location.

--- graphview_example_ib/GraphView/GraphViewController.swift
+++ graphview_example_ib/GraphView/GraphViewController.swift
@@ -5,18 +5,22 @@
 
 import UIKit
 
-class GraphViewController: UIViewController {
+class GraphViewController: UIViewController, PointSelectedProtocol {
     @IBOutlet var graphView: ScrollableGraphView?
     var data: [Double]?
     var labels: [String]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        graphView?.pointSelectedDelegate = self
         guard let data = data, let labels = labels else {
             return
         }
         graphView?.set(data: data, withLabels: labels)
+    }
+    
+    func pointWasSelectedAt(label: String, value: Double, location: CGPoint) {
+        print("Point selected x:\(label) y:\(value) point:\(location)\n")
     }
 
 }
