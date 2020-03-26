1f2a1a4c02582ff2524a36db58a972321205d676
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2020-03-26 18:20:02 +0100

    Set swift package manager library type to dynamic.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(name: "ScrollableGraphView",
                       platforms: [.iOS(.v8)],
                       products: [.library(name: "ScrollableGraphView",
+                                          type: .dynamic,
                                           targets: ["ScrollableGraphView"])],
                       targets: [.target(name: "ScrollableGraphView",
                                         path: "Classes")],
