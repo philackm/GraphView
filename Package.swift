34e045aac37fe977d2c7e191e9efd40b11f230c4
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2020-05-03 14:53:26 +0200

    Removed package manager library type

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 let package = Package(name: "ScrollableGraphView",
                       platforms: [.iOS(.v8)],
                       products: [.library(name: "ScrollableGraphView",
-                                          type: .dynamic,
                                           targets: ["ScrollableGraphView"])],
                       targets: [.target(name: "ScrollableGraphView",
                                         path: "Classes")],
